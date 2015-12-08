7eb136e9eee47b0386ac83122a1e575a470eda83
Alen <ahrga93@gmail.com>
2015-12-09 00:09:06 +0100

    Ažuriran IndexKontroler.java Dodan MediaType te promjenjena funkcija prikaziIndex().

--- sza-webapp/src/main/java/hr/fer/pipp/sza/webapp/kontroleri/IndexKontroler.java
+++ sza-webapp/src/main/java/hr/fer/pipp/sza/webapp/kontroleri/IndexKontroler.java
@@ -3,19 +3,21 @@ package hr.fer.pipp.sza.webapp.kontroleri;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.mvc.Viewable;
 
 @Path("/")
 public class IndexKontroler {
 
 	@GET
-	public void prikaziIndex(@Context HttpServletRequest request, @Context HttpServletResponse response)
-			throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(request, response);
+	@Produces(MediaType.TEXT_HTML)
+	public Response prikaziIndex() throws ServletException, IOException {
+		return Response.ok(new Viewable("/index")).build();
 	}
 
 }
