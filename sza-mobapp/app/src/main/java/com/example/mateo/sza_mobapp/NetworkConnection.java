4d16995d6616d0b4556f687b32da5819e1fbab4e
Mateo <mateo.glavicic@gmail.com>
2016-01-12 20:04:14 +0100

    android izmjene

--- sza-mobapp/app/src/main/java/com/example/mateo/sza_mobapp/NetworkConnection.java
+++ sza-mobapp/app/src/main/java/com/example/mateo/sza_mobapp/NetworkConnection.java
@@ -15,6 +15,9 @@ import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.ResponseHandlerInterface;
 import com.loopj.android.http.SyncHttpClient;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
 import org.json.JSONObject;
@@ -26,9 +29,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 import javax.security.auth.callback.Callback;
@@ -36,8 +41,8 @@ import javax.security.auth.callback.Callback;
 /**
  * Created by Mateo on 15.12.2015..
  */
-public class NetworkConnection {
-    private  String urlString= "http://192.168.1.102:8080/sza-webapp/android/";
+public class NetworkConnection extends AsyncTask<String, Void, String> {
+    private  String urlString= "http://10.129.46.220:8080/sza-webapp/android/";
     private URL url;
     //"http://localhost:8080/sza-webapp/android/";
     //String URL="http://gurujsonrpc.appspot.com/guru";
@@ -45,7 +50,26 @@ public class NetworkConnection {
      Context context;
      OnJSONResponseCallback callback;
     JSONObject odg;
-    StringEntity entity;
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        // params comes from the execute() call: params[0] is the url.
+        //try {
+            return asd(urls[0]);
+        //} catch (IOException e) {
+          //  return "Unable to retrieve web page. URL may be invalid.";
+        //}
+    }
+    // onPostExecute displays the results of the AsyncTask.
+
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        Log.d("ODGOVOR",result);
+
+    }
 
     public interface OnJSONResponseCallback {
         public void onJSONResponse(boolean success, JSONObject response);
@@ -56,35 +80,22 @@ public class NetworkConnection {
         this.callback = callback;
     }
 
-    public JSONObject provjeri(Korisnik korisnik, OnJSONResponseCallback callback1, Context context){
+    public JSONObject provjeri(String korisnik, OnJSONResponseCallback callback1, Context context){
         AsyncHttpClient httpClient = new AsyncHttpClient();
         callback = callback1;
         //context = context;
         RequestParams params = new RequestParams();
+        params.put("json", korisnik);
 
-        /*Http entity = new ByteArrayEntity(korisnik.toString().getBytes("UTF-8"));
-        entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-        try {
-            entity = new StringEntity(korisnik);
-            url= new URL(urlString);
-        }catch (UnsupportedEncodingException e){
-            e.printStackTrace();
-        }catch (MalformedURLException m){
-            m.printStackTrace();
-        }
-        entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));*/
-        params.add("korisnickoime", korisnik.getIme());
-        params.add("lozinka", korisnik.getLozinka());
-        Log.d("added params", korisnik.getIme());
 
-        httpClient.post(context, urlString,  params, new JsonHttpResponseHandler() {
+        JsonHttpResponseHandler jsonHandler = new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(JSONObject jsonObject) {
                 Log.d("*****JsonObject", jsonObject.toString());//<-Json objekt koji dobijes kao response
                 //UpdateData(jsonObject);
                 callback.onJSONResponse(true, jsonObject);
-                odg=jsonObject;
+                odg = jsonObject;
             }
 
             @Override
@@ -94,9 +105,56 @@ public class NetworkConnection {
                 odg = error;
             }
 
-        });
+        };
+        httpClient.post(context, urlString, params, jsonHandler);
+        InputStream is = null;
 
         return odg;
+    }
+
+
+    protected String asd(String podaci){
+        String output = "";
+        try {
+            Log.d("pocetak asd","");
+            URL url = new URL("http://10.129.46.220:8080/sza-webapp/android/");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            //String input = "{\"ime\":\"korisnik123\",\"lozinka\":\"lozinka123\"}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(podaci.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                Log.d("failed", "asd");
+                /*throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());*/
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                Log.d("asdf",output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        return output;
     }
 
 
@@ -106,24 +164,6 @@ public class NetworkConnection {
         Log.d("*****UhvacenOdgovor", json);
         prihvaceno = true;
     }*/
-
-
-
-
-    /*StringEntity se = null;
-    try {
-        se = new StringEntity(params.toString());
-    } catch (UnsupportedEncodingException e) {
-        // handle exceptions properly!
-    }
-    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-
-    client.post(null, "www.example.com/objects", se, "application/json", responseHandler);*/
-
-
-
-
-
 
 
     public boolean isConnected(){
